9d53a8e954deaeadc81d1b3e49fe177f6df43a75
Steve BERENGER <steve.berenger@gmail.com>
2022-06-14 18:34:35 +0200

    insert produit

--- food-db/data.dml.sql
+++ food-db/data.dml.sql
@@ -26,42 +26,14 @@ true,
 'https://fr.openfoodfacts.org/images/products/762/221/024/5281/front_fr.15.full.jpg',
 1,
 1
-);
-
-/*INSERT INTO products (barcode,designation,lactose,gluten,photo_link,category_id,brand_id) VALUES
-('5000112547412', 
-'Coca cola', 
-false, 
+),('5000112547412', 
+'Coca cola',
 false,
-'6'
-'5'
-'https://images.openfoodfacts.org/images/products/500/011/254/7412/front_de.30.100.jpg');
-
-INSERT INTO products (barcode,designation,lactose,gluten,photo_link,category_id,brand_id) VALUES
-('3017760002035', 
-'Petit écolier Chocolat fin ', 
-true, 
-true,
-'1'
-'1'
-'https://fr.openfoodfacts.org/images/products/301/776/000/2035/front_fr.34.full.jpg');
-
-
-INSERT INTO products (barcode,designation,lactose,gluten,photo_link,category_id,brand_id) VALUES
-('8076809513388 ', 
-'Barilla sauce tomates arrabbiata 400g - 400 g', 
-false, 
 false,
-'5',
-'2',
-'https://fr.openfoodfacts.org/images/products/807/680/951/3388/front_fr.184.400.jpg');
+'https://images.openfoodfacts.org/images/products/500/011/254/7412/front_de.30.100.jpg',
+4,
+6
+),('3017760002035','Petit écolier Chocolat fin',true,true,'https://fr.openfoodfacts.org/images/products/301/776/000/2035/front_fr.34.full.jpg',
+1,1),('8076809513388','Barilla sauce tomates arrabbiata 400g',false,false,'https://fr.openfoodfacts.org/images/products/807/680/951/3388/front_fr.184.400.jpg',4,2),
+('8000500310427','Nutella Biscuit',true,true,'https://fr.openfoodfacts.org/images/products/800/050/031/0427/ingredients_fr.95.400.jpg',4,3);
 
-INSERT INTO products (barcode,designation,lactose,gluten,photo_link,category_id,brand_id) VALUES
-('8000500310427', 
-'Nutella Biscuits biscuits fourrés pâte à tartiner et cacao x22 - 304 g', 
-true, 
-true,
-4,
-4,
-'https://fr.openfoodfacts.org/images/products/800/050/031/0427/ingredients_fr.95.400.jpg');
-*/
